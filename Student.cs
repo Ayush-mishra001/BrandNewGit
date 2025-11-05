953c5e3343b4741a7444acce0885b8d1446fa5f7
Ayush-mishra001 <ayushgpt4321@gmail.com>
2025-11-05 17:42:53 +0530

    add new field

--- Student.cs
+++ Student.cs
@@ -4,4 +4,5 @@ public class Student
     public string Name { get; set; }
     public string Description { get; set; }
 
+
 }
---
+++ user.cs
@@ -0,0 +1,8 @@
+internal class
+ {
+    public string MyProperty { get; set; }
+    public string MyProperty2 { get; set; }
+    public string MyProperty3 { get; set;
+
+
+}
